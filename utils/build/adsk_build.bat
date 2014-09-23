67818b72579e7fbd4947c2cd4ca6271ca3ed88b7
stanevt <traian.stanev@autodesk.com>
2014-09-23 13:17:19 -0400

    Add adsk specific build files.

---
+++ utils/build/adsk_build.bat
@@ -0,0 +1,2 @@
+python build.py --include adsk_common --include adsk_extras --output ../../build/adsk_three.js
+python build.py --include adsk_common --include adsk_extras --minify --output ../../build/adsk_three.min.js
